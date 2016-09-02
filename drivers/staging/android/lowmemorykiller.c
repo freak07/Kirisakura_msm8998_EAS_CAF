5c761aed7e1ec8fa258ef5db3789c7f9cb7fe7be
Tim Murray <timmurray@google.com>
2016-09-02 16:04:41 -0700

    ANDROID: lowmemorykiller: account for unevictable pages
    lowmemorykiller was not taking into account unevictable pages when
    deciding what level to kill. If significant amounts of memory were
    pinned, this caused lowmemorykiller to effectively stop at a much higher
    level than it should.
    
    bug 31255977
    
    Change-Id: I763ecbfef8c56d65bb8f6147ae810692bd81b6e2
    Signed-off-by: Tim Murray <timmurray@google.com>
    (cherry picked from commit 16cfd1e483b9fc7c8f44d0a16442eb1eef168acf)
    (cherry picked from commit 4c691887b37185556a9c7f9f91d1705c4ee823a6)
    (cherry picked from commit eb1cc56306f5b7e219c1ede6fc1afdb81dd77287)

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -430,11 +430,11 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 
 	other_free = global_page_state(NR_FREE_PAGES);
 
-	if (global_page_state(NR_SHMEM) + global_page_state(NR_MLOCK) + total_swapcache_pages() <
+	if (global_page_state(NR_SHMEM) + total_swapcache_pages() <
 		global_page_state(NR_FILE_PAGES) + zcache_pages())
 		other_file = global_page_state(NR_FILE_PAGES) + zcache_pages() -
 						global_page_state(NR_SHMEM) -
-						global_page_state(NR_MLOCK) -
+						global_page_state(NR_UNEVICTABLE) -
 						total_swapcache_pages();
 	else
 		other_file = 0;
