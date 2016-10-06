d896abcb0a6529c9b573e5766d0ae9b614a4f3a4
Dmitry Torokhov <dtor@chromium.org>
2016-10-06 15:53:38 -0700

    CHROMIUM: remove Android's cgroup generic permissions checks
    The implementation is utterly broken, resulting in all processes being
    allows to move tasks between sets (as long as they have access to the
    "tasks" attribute), and upstream is heading towards checking only
    capability anyway, so let's get rid of this code.
    
    BUG=b:31790445,chromium:647994
    TEST=Boot android container, examine logcat
    
    Change-Id: I2f780a5992c34e52a8f2d0b3557fc9d490da2779
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/394967
    Reviewed-by: Ricky Zhou <rickyz@chromium.org>
    Reviewed-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: joshuous <joshuous@gmail.com>
    (cherry picked from commit 7482389c4bfbe1b4828d1909e243c5ff39527e85)

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4963,11 +4963,6 @@ static int mem_cgroup_can_attach(struct cgroup_taskset *tset)
 	return ret;
 }
 
-static int mem_cgroup_allow_attach(struct cgroup_taskset *tset)
-{
-	return subsys_cgroup_allow_attach(tset);
-}
-
 static void mem_cgroup_cancel_attach(struct cgroup_taskset *tset)
 {
 	if (mc.to)
@@ -5119,10 +5114,6 @@ static void mem_cgroup_move_task(void)
 }
 #else	/* !CONFIG_MMU */
 static int mem_cgroup_can_attach(struct cgroup_taskset *tset)
-{
-	return 0;
-}
-static int mem_cgroup_allow_attach(struct cgroup_taskset *tset)
 {
 	return 0;
 }
@@ -5343,7 +5334,6 @@ struct cgroup_subsys memory_cgrp_subsys = {
 	.css_reset = mem_cgroup_css_reset,
 	.can_attach = mem_cgroup_can_attach,
 	.cancel_attach = mem_cgroup_cancel_attach,
-	.allow_attach = mem_cgroup_allow_attach,
 	.post_attach = mem_cgroup_move_task,
 	.bind = mem_cgroup_bind,
 	.dfl_cftypes = memory_files,
