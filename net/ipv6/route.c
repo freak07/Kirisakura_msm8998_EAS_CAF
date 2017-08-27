2e042585e3d7985edf8e178602a699012250e64e
tomascus <arbiter1000@gmail.com>
2017-08-27 11:29:37 +1000

    net: import HTC changes
    Change-Id: Ia50108c0dc5efd45e1a727372f94713bd32b4817
    (cherry picked from commit 7fcccf24ea0b5ac0746559e0cfc34e9684152b2c)

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -3334,6 +3334,31 @@ void inet6_rt_notify(int event, struct rt6_info *rt, struct nl_info *info,
 		kfree_skb(skb);
 		goto errout;
 	}
+
+#ifdef CONFIG_HTC_NETWORK_MODIFY
+    if((rt->rt6i_dst.addr.s6_addr32[0] == 0x0) && (rt->rt6i_dst.addr.s6_addr32[1] == 0x0) &&
+       (rt->rt6i_dst.addr.s6_addr32[2] == 0x0) && (rt->rt6i_dst.addr.s6_addr32[3] == 0x0))  // default route
+    {
+        pr_info("[NET]%s: process:%s(pid:%d), parent:%s(pid:%d), rt=%p, rt->rt6i_dst.addr:%pI6, rt->rt6i_idev->dev->name:%s, rt->rt6i_gateway:%pI6, rt->rt6i_table->tb6_id:%d, event:%s\n",
+            __func__,
+            current->comm,
+            current->pid,
+            (current->parent) ? current->parent->comm : "(Invalid)",
+            (current->parent) ? current->parent->pid : 0,
+            rt,
+            &rt->rt6i_dst.addr,
+            rt->rt6i_idev->dev->name,
+            &rt->rt6i_gateway,
+            rt->rt6i_table->tb6_id,
+            (event == RTM_NEWROUTE) ? "RTM_NEWROUTE" : ((event == RTM_DELROUTE) ? "RTM_DELROUTE" : "UNKNOWN")
+            );
+
+        pr_info("[NET]%s: ---- dump call stack start ----\n", __func__);
+        dump_stack();
+        pr_info("[NET]%s: ---- dump call stack end ----\n", __func__);
+    }
+#endif
+
 	rtnl_notify(skb, net, info->portid, RTNLGRP_IPV6_ROUTE,
 		    info->nlh, gfp_any());
 	return;
