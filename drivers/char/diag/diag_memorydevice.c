4397474809b80e6bd0a33006ec47797de98edb9a
tomascus <arbiter1000@gmail.com>
2017-08-27 10:46:44 +1000

    drivers: char: import HTC changes
    Change-Id: I60ed3ecd8b1cc8cddd668076672a6d12815a16f8

--- drivers/char/diag/diag_memorydevice.c
+++ drivers/char/diag/diag_memorydevice.c
@@ -196,8 +196,8 @@ int diag_md_write(int id, unsigned char *buf, int len, int ctx)
 			continue;
 
 		found = 1;
-		driver->data_ready[i] |= USER_SPACE_DATA_TYPE;
-		pr_debug("diag: wake up logging process\n");
+		driver->data_ready[i] |= USERMODE_DIAGFWD;
+		DIAG_DBUG("diag: wake up logging process\n");
 		wake_up_interruptible(&driver->wait_q);
 	}
 
