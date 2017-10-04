2677aeaf87bac343ce09a8673316558365d3a8db
freak07 <michalechner92@googlemail.com>
2017-10-04 21:43:50 +0200

    Revert "sched/fair: add absolute energy normalization"
    This reverts commit 66a3cc555296fedf90170984ef975514f69de5e0.

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -18,6 +18,7 @@ bool schedtune_initialized = false;
 unsigned int sysctl_sched_cfs_boost __read_mostly;
 
 extern struct reciprocal_value schedtune_spc_rdiv;
+struct target_nrg schedtune_target_nrg;
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 unsigned int top_app_idx = 0;
