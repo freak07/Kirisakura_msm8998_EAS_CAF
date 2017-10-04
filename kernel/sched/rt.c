83da2ea827090e96a6e17c698635d478ace8032a
freak07 <michalechner92@googlemail.com>
2017-10-04 21:44:04 +0200

    Revert "sched: Use schedtune boosted WALT signal for RT"
    This reverts commit 7f1773b24278ed77495a7e1d9d0c8af1666cdadb.

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1636,34 +1636,14 @@ static void check_preempt_curr_rt(struct rq *rq, struct task_struct *p, int flag
 }
 
 #ifdef CONFIG_SMP
-
-static void sched_rt_update_capacity_req(struct rq *rq, bool tick)
+static void sched_rt_update_capacity_req(struct rq *rq)
 {
 	u64 total, used, age_stamp, avg;
 	s64 delta;
-	int cpu = cpu_of(rq);
 
 	if (!sched_freq())
 		return;
 
-#ifdef CONFIG_SCHED_WALT
-	if (!walt_disabled && sysctl_sched_use_walt_cpu_util) {
-		unsigned long cpu_utilization = boosted_cpu_util(cpu);
-		unsigned long capacity_curr = capacity_curr_of(cpu);
-		int req = 1;
-
-		/*
-		 * During a tick, we don't throttle frequency down, just update
-		 * the rt utilization.
-		 */
-		if (tick && cpu_utilization <= capacity_curr)
-			req = 0;
-
-		set_rt_cpu_capacity(cpu, req, cpu_utilization);
-
-		return;
-	}
-#endif
 	sched_avg_update(rq);
 	/*
 	 * Since we're reading these variables without serialization make sure
@@ -1682,10 +1662,10 @@ static void sched_rt_update_capacity_req(struct rq *rq, bool tick)
 	if (unlikely(used > SCHED_CAPACITY_SCALE))
 		used = SCHED_CAPACITY_SCALE;
 
-	set_rt_cpu_capacity(cpu, 1, (unsigned long)(used));
+	set_rt_cpu_capacity(rq->cpu, 1, (unsigned long)(used));
 }
 #else
-static inline void sched_rt_update_capacity_req(struct rq *rq, bool tick)
+static inline void sched_rt_update_capacity_req(struct rq *rq)
 { }
 
 #endif
@@ -1766,7 +1746,7 @@ pick_next_task_rt(struct rq *rq, struct task_struct *prev)
 		 * This value will be the used as an estimation of the next
 		 * activity.
 		 */
-		sched_rt_update_capacity_req(rq, false);
+		sched_rt_update_capacity_req(rq);
 		return NULL;
 	}
 
@@ -2496,7 +2476,7 @@ static void task_tick_rt(struct rq *rq, struct task_struct *p, int queued)
 	update_curr_rt(rq);
 
 	if (rq->rt.rt_nr_running)
-		sched_rt_update_capacity_req(rq, true);
+		sched_rt_update_capacity_req(rq);
 
 	watchdog(rq, p);
 
